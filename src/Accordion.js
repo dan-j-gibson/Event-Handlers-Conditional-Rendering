999c5238619e5ed4ad3d8dce015dd3d765ce0b83
Dan Gibson <dgibson1124@gmail.com>
2020-11-04 23:54:27 -0800

    secondary commit

---
+++ src/Accordion.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Accordion.css';
+
+export default class Accordion extends Component {
+    static defaultProps = {
+      sections: []
+    };
+
+    state = {
+        activeSectionIndex: null,
+      }
+
+    handleSetActiveSecion = (sectionIndex) => {
+    this.setState({ activeSectionIndex: sectionIndex })
+    }
+    
+    renderItem(section, idx, activeSectionIndex) {
+        return (
+          <li className='Accordion__item' key={idx}>
+            <button
+              type='button'
+              onClick={() => this.handleSetActiveSecion(idx)}
+            >
+              {section.title}
+            </button>
+            {(activeSectionIndex === idx) && <p>{section.content}</p>}
+          </li>
+        )
+      }
