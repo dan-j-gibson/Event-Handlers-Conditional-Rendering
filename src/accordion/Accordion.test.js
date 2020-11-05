3b55099fc86e6fa59dfcdb4502a8a2b89196f845
Dan Gibson <dgibson1124@gmail.com>
2020-11-05 00:06:07 -0800

    add content and css
    - add sections content
    - update CSS

---
+++ src/accordion/Accordion.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Accordion from './Accordion'
+
+const sections = [
+    {
+      title: 'Section 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Section 2',
+      content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    },
+    {
+      title: 'Section 3',
+      content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    },
+  ]
+  // e.g. <Accordion sections={sections} />
