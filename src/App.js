c71a00663884122b5bbff091429a03c708e9430e
Dan Gibson <dgibson1124@gmail.com>
2020-11-05 00:18:49 -0800

    final commit minus snapshots
    - unable to get snapshots to work. Might have to install one more testing library

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import React from 'react';
 import HelloWorld from './HelloWorld'
 import Bomb from './Bomb';
 import RouletteGun from './RouletteGun';
+import Accordion from './accordion/Accordion'
+import './App.css'
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 function App() {
   return (
     <>
+      <h2>Hello World!</h2>
       <HelloWorld />
         <br></br>
+        <h2>Bomb</h2>
        <Bomb />
        <br></br>
+       <h2>Roulette Gun</h2>
        <RouletteGun />
+       <br></br>
+       <h2>Accordion</h2>
+        <Accordion sections={sections} />
     </>
   );
 }
--- src/accordion/Accordion.test.js
+++ src/accordion/Accordion.test.js
@@ -18,3 +18,28 @@ const sections = [
     },
   ]
   // e.g. <Accordion sections={sections} />
+
+  describe(`Accordion Component`, () => {
+    it('renders empty given no Accordion without errors', () => {
+      const wrapper = shallow(<Accordion />)
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+it('renders the no sections by default', () => {
+    const wrapper = shallow(<Accordion sections={sectionsProp} />)
+    expect(toJson(wrapper)).toMatchSnapshot() 
+    })
+
+    it('opens any clicked section', () => {
+    const wrapper = shallow(<Accordion sections={sectionsProp} />)
+    wrapper.find('button').at(1).simulate('click')
+    expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('only opens one section at a time', () => {
+    const wrapper = shallow(<Accordion sections={sectionsProp} />)
+    wrapper.find('button').at(1).simulate('click')
+    wrapper.find('button').at(2).simulate('click')
+    expect(toJson(wrapper)).toMatchSnapshot()
+    })
+})
